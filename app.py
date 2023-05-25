8d12e6776ba3df1def9e39f6f9aa4415409f8f9b
damian <ddr.zim@hotmail.com>
2023-05-25 00:09:46 -0600

    I wrote comments in my code and create the file requirements.txt

--- app.py
+++ app.py
@@ -10,7 +10,5 @@ from routes import blp as CalculatorBlueprint
 def create_app():
     app = Flask(__name__)
     
-    app.config["SECRET_KEY"] = "123"
     app.register_blueprint(CalculatorBlueprint)
-    
     return app
--- functions.py
+++ functions.py
@@ -7,6 +7,12 @@ from num2words import num2words
 # ****************************************************************************
 
 def operatorConversor(operator):
+    '''This functions helps to
+    map an specific 'operator name'
+    to its symbol, wich will be the
+    real character to show in the
+    screen.'''
+    
     if operator == "add":
         return {"name": "add", "symbol": "+"}
     elif operator == "substract":
@@ -29,15 +35,32 @@ def operatorConversor(operator):
         return {"name": "comma", "symbol": ","}
     
 def numToWords(number, language):
+    '''This function turns a string 
+    number into the textual writting 
+    form of that number
+    
+    We use the library 'num2words'
+    which receives as parameter a 
+    float or int number, so the first
+    step is to convert our string
+    number to one of them.
+    '''
+    
+    # Transform the string into a float
+    # if there is a dot in it
     if "." in number:
         casted_number = float(number)
+    # Transform the string into an int
+    # if there is no dots in it
     else:
         casted_number = int(number)
     
+    # Translate the number to text in English
     if language == "en":
         english_string = num2words(casted_number, lang="en")
         return english_string
     
+    # Translate the number to text in Spanish
     if language == "es":
         spanish_string = num2words(casted_number, lang="es")
         return spanish_string
